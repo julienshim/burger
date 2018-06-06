ca5f5a892184e3e1399dcec683be90ad188d5daa
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-05 21:52:09 -0700

    added comments to respective files per hw instructions as a tracker. set up server and mysql db

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-var express = require("express");
-var methodOverride = require('method-override')
-var bodyParser = require("body-parser");
+// Require the following npm packages inside of the server.js file:
+var express = require("express"); //express
+var methodOverride = require('method-override') //method-override
+var bodyParser = require("body-parser"); // body-parser
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
